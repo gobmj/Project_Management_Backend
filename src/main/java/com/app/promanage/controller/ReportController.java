5eb7d515055adaa738b187383f8110cfd73f7aac
I529003 <govind.m.j@sap.com>
2025-06-01 12:27:44 +0530

    chore: email revert

--- src/main/java/com/app/promanage/controller/ReportController.java
+++ src/main/java/com/app/promanage/controller/ReportController.java
@@ -35,11 +35,6 @@ public class ReportController {
             @RequestParam String email
     ) {
         try {
-            // Append @gmail.com if not present
-            if (!email.contains("@")) {
-                email += "@gmail.com";
-            }
-
             byte[] reportPdf = reportService.generateProjectReport(projectId, type);
             emailService.sendEmailWithAttachment(
                     email,
